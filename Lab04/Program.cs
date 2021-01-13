0e07e6d746076f6a1247df42c906fa1bdb6936ae
康文龍 <freeman6@gmail.com>
2021-01-13 14:39:18 +0800

    第三組 DI 範例-在元件庫中使用 Microsoft.Extensions.DependencyInjection

---
+++ Lab04/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using ServiceLibrary;
+
+namespace Lab04
+{
+    class Program
+    {
+        private readonly IService serviceA;
+
+        public Program(IService serviceA)
+        {
+            this.serviceA = serviceA;
+        }
+
+        static void Main(string[] args)
+        {
+            // Step 01 建立 Service Collection
+            var services = new ServiceCollection();
+
+            // Step 02 加入服務
+            services.AddTransient<Program>();
+            services.AddServiceAB();
+
+            // Step 03 建立 ServiceProvider
+            var serviceProvider = services.BuildServiceProvider();
+
+            // Step 04 取得相關服務
+            serviceProvider.GetRequiredService<Program>().Run();
+        }
+
+        public void Run()
+        {
+            serviceA.DoSomething();
+        }
+
+    }
+
+}
---
+++ ServiceLibrary/CommonService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace ServiceLibrary
+{
+    public static class IServiceCollectionExtension
+    {
+        public static IServiceCollection AddServiceAB(this IServiceCollection services)
+        {
+            services.AddLogging(config => config.AddConsole());
+            services.AddTransient<IService, ServiceA>();
+            services.AddTransient<IService, ServiceB>();
+            return services;
+        }
+    }
+
+    public interface IService
+    {
+        void DoSomething();
+    }
+
+    public class ServiceA : IService
+    {
+        private readonly ServiceB serviceB;
+        public ServiceA(ServiceB serviceB)
+        {
+            this.serviceB = serviceB;
+        }
+
+        public void DoSomething()
+            => serviceB.DoSomething();
+    }
+
+    public class ServiceB : IService
+    {
+        private readonly ILogger<ServiceB> logger;
+        public ServiceB(ILogger<ServiceB> logger)
+        {
+            this.logger = logger;
+        }
+        public void DoSomething()
+            => logger.LogInformation("ServiceB is doing something.");
+    }
+
+}
